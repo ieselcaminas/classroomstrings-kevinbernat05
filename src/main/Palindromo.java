3e5ff94d4e3bb4a5421264816d1cd23432e38e9b
kevromber <kevromber@gva.alu.edu.es>
2024-11-15 10:33:15 +0100

    Strings

--- src/main/Palindromo.java
+++ src/main/Palindromo.java
@@ -12,6 +12,6 @@ public class Palindromo {
     }
 
     public static void main(String[] args) {
-        System.out.println(esPalindromo("abcba"));
+        System.out.println(esPalindromo("abccba"));
     }
 }
